5271e1e4c16868622c62923d3c25f3a0b18b4194
Carlos Raez <carlosraez@gmail.com>
2021-10-05 13:02:15 +0200

    Fix name css home
    Fix css name

---
+++ src/Components/timer/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Timer = () => {
+	return (
+		<div>
+			<div>00 days</div>
+			<div>00 hours</div>
+			<div>00 minutes</div>
+			<div>00 seconds</div>
+		</div>
+	);
+};
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, /*useSelector  */} from 'react-redux';
+
 import { finishLogged } from '../../actions/actions';
+import { Timer } from '../../Components/timer/Timer';
 
 
 export const UsersScreen = () => {
 
 	const dispatch = useDispatch();
+	//const { name } = useSelector(state => state.auth);
 
 	const  handleLogout = () => {
 		dispatch( finishLogged() );
 	};
 
+	const name  = 'Carlos';
+
 	return (
 		<>
 			<header className="container">
-				<h1>Welcome</h1>
+				<h1>Welcome {name}!</h1>
 				<p>The last time you accessed was</p>
 			</header>
 			<main>
-
+				<Timer />
 			</main>
 			<footer>
 				<button className="btn btn-primary" onClick={handleLogout}>Logout</button>
