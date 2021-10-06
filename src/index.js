8925d9a63b4d227242e6e49f59f1920076b08478
Carlos Raez <carlosraez@gmail.com>
2021-10-06 16:41:38 +0200

    ADD Service Worker to PWA
    ADD Service Worker to PWA

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './styles/styles.scss';
 
 ReactDOM.render(
-
 	<App />,
 	document.getElementById('root')
 );
+
+serviceWorkerRegistration.unregister();
